725bc730ae20d146d4cb18788b38e601beaa1706
halcyoona <halcyoona@gmail.com>
2019-05-06 11:41:39 +0500

    data

---
+++ getting_started/data.py
@@ -0,0 +1,26 @@
+def Articles():
+	articles = [
+		{
+			'id': 1,
+			'title': "Article One",
+			'body': "Lorem Ipmus",
+			'author': "Mehmood",
+			'create_date':" 03-05-2019"
+		},
+		{
+			'id': 1,
+			'title': "Article One",
+			'body': "Lorem Ipmus",
+			'author': "Hussain",
+			'create_date':" 03-05-2019"
+		},
+		{
+			'id': 1,
+			'title': "Article One",
+			'body': "Lorem Ipmus",
+			'author': "Hassan",
+			'create_date':" 03-05-2019"
+		}
+
+	]
+	return articles;
