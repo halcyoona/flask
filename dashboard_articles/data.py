20b6c9be1d4b91e9dfd3ff40d2189bffe5a451d9
halcyoona <halcyoona@gmail.com>
2019-05-10 10:20:28 +0500

    data

---
+++ dashboard_articles/data.py
@@ -0,0 +1,26 @@
+def Articles():
+	articles = [
+		{
+			'id': 1,
+			'title': "Article One",
+			'body': "Lorem Ipmus",
+			'author': "Mehmood",
+			'create_date':" 03-05-2019"
+		},
+		{
+			'id': 2,
+			'title': "Article Two",
+			'body': "Lorem Ipmus",
+			'author': "Hussain",
+			'create_date':" 03-05-2019"
+		},
+		{
+			'id': 3,
+			'title': "Article Three",
+			'body': "Lorem Ipmus",
+			'author': "Hassan",
+			'create_date':" 03-05-2019"
+		}
+
+	]
+	return articles;
